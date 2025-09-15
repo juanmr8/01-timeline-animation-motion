de0ba5f48e7d0866bbb94a3efdfb30cf4da6181e
Juan Mora Romero <moraromerojuan8@gmail.com>
2025-09-15 20:11:49 +0200

    animation partially completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,255 @@
-"use client";
-import {motion} from "motion/react";
-import { useAnimate, usePresence } from "framer-motion"
-import { useEffect, useRef } from "react";
-import { timeline } from "@/app/utils";
-import { div } from "framer-motion/client";
+'use client';
+import { AnimationOptions, motion, motionValue } from 'motion/react';
+import { useMotionTimeline } from './use-motion-timeline';
+
+
+export const TRANSITION: AnimationOptions = {
+  duration: 1.5,
+  delay: 0.25,
+  ease: [0.76, 0, 0.24, 1],
+};
 
 export default function Home() {
-	const [scope, animate] = useAnimate();
-	const [isPresent, safeToRemove] = usePresence();
-	const elementRefs = useRef<any>(new Map());
-	useEffect(() => {
-		if (isPresent) {
-			void timeline(animate, elementRefs);
-		}
-	}, [isPresent, animate]);
+  const scope = useMotionTimeline([
+    // Keyframe 1
+    [
+      ['.left-wall', { width: 'calc(50% - 200px)' }, { ...TRANSITION }],
+      ['.right-wall', { width: 'calc(50% - 200px)' }, { ...TRANSITION }],
+      ['.with-no-hidden', { x: '-10%' }, { ...TRANSITION }],
+    ],
+    // Keyframe 2
+    [
+      ['.left-wall-separator', { width: '50%' }, { ...TRANSITION }],
+      ['.location', { x: '-100%' }, { ...TRANSITION }],
+      ['.location-hidden', { x: '50%' }, { ...TRANSITION }],
+      ['.with-no', { x: '155%' }, { ...TRANSITION }],
+      ['.with-no-hidden', { x: '100%' }, { ...TRANSITION }],
+      ['.limits', { x: '-105%' }, { ...TRANSITION }],
+      ['.limits-hidden', { x: '0%' }, { ...TRANSITION }],
+      ['.input-output', { x: '110%' }, { ...TRANSITION }],
+      ['.input-output-hidden', { x: '50%' }, { ...TRANSITION }],
+    ],
+
+    // Keyframe 3
+    [
+      ['.left-wall-separator', { width: '100%' }, { ...TRANSITION }],
+      ['.location', { right: '25%' }, { ...TRANSITION }],
+    ],
+
+    // Keyframe 4
+    [
+      ['.top-wall', { y: '-100%' }, { ...TRANSITION }],
+      ['.left-wall ', { x: '-100%' }, { ...TRANSITION }],
+      ['.bottom-wall', { y: '100%' }, { ...TRANSITION }],
+      ['.right-wall', { x: '100%' }, { ...TRANSITION }],
+    ],
+  ]);
+
   return (
-	<div ref={scope}>
-<div  className="grid grid-cols-12 grid-rows-6 relative h-screen overflow-hidden
-	text-[#252525] px-[32px] py-[32px]">{/*  */}
-		<div className=" col-span-12 grid grid-cols-12">
-			<motion.h1 ref={el => elementRefs.current.set('create', el!)}
- className="title col-start-4 col-span-4">
-				Create
-			</motion.h1>
-		</div>
-		<div className="col-span-12 grid grid-cols-12">
-			<motion.p ref={el => elementRefs.current.set('location', el!)} className="subtitle col-span-4 col-start-5 self-end">MA, ES</motion.p>
-			<motion.p ref={el => elementRefs.current.set('location-hidden', el!)} className="subtitle col-start-9 col-span-4 self-end" style={{ x: "120%" }}>MA, ES</motion.p>
-		</div>
-		<div className="col-span-12 grid grid-cols-12">
-			<motion.h1 ref={el => elementRefs.current.set('with-no-hidden', el!)} style={{ x: "-120%" }} className="title col-span-4">With No</motion.h1>
-			<motion.h1 ref={el => elementRefs.current.set('with-no', el!)} className="title col-span-4 col-start-7">With No</motion.h1>
-		</div>
-		<div className="col-span-12 grid grid-cols-12 self-end" >
-			<motion.p ref={el => elementRefs.current.set('input-output', el!)} style={{ x: "-160%"}} className="subtitle col-start-2 col-span-2">0 -&gt; 1</motion.p>
-			<motion.p ref={el => elementRefs.current.set('input-output-hidden', el!)} className="subtitle col-start-9 col-span-2" style={{ x: "0%" }}>0 -&gt; 1</motion.p>
-		</div>
-		<div className="col-span-12 grid grid-cols-12">
-			<motion.h1 ref={el => elementRefs.current.set('limits', el!)} className="title col-start-3 col-span-4">Limits</motion.h1> {/*  */}
-			<motion.h1 ref={el => elementRefs.current.set('limits-hidden', el!)} style={{ x: "100%" }} className="title col-start-9 col-span-4">Limits</motion.h1>
-		</div>
-
-		<div className="col-span-12 grid grid-cols-12 self-end">
-			<motion.p ref={el => elementRefs.current.set('copyright', el!)} className="subtitle col-start-6">© INC.</motion.p>
-			<motion.p className="subtitle">SINCE 1999</motion.p>
-		</div>
-
+    <div ref={scope}>
+      <div className='relative h-screen overflow-hidden text-[#252525]'>
+        <video
+          src='/output.mp4'
+          autoPlay
+          muted
+          loop
+          className="video"
+        />
+        <div className="absolute inset-0 z-0 bg-gradient-to-b from-black/80 via-black/60 to-black/80 pointer-events-none" />
+        <div className='wall-container relative h-full w-full'>
+          <TopWall />
+          <LeftWall />
+          <BottomWall />
+          <RightWall />
+        </div>
+      </div>
     </div>
-	<div className="absolute w-screen h-screen inset-0 flex  pointer-events-none">
-			<motion.div ref={el => elementRefs.current.set('bg', el!)} style={{ width: "0%", height: "75%" }} className="overflow-hidden m-auto rounded-lg"><img className=" inset-0 w-screen h-screen object-cover" src="/img2.jpg" alt="bg" /></motion.div>
-		</div>
-	</div>
-
   );
+}
+
+const RightWall = () => {
+  return (
+    <motion.div
+      className='right-wall absolute top-0 h-full bg-white'
+      style={{
+        right: '0',
+        width: 'calc(50% - 0px)',
+      }}
+    >
+      <div className='px-4'>
+        {/** Line 1 */}
+        <motion.h1 className='create-title title text-right opacity-0'>
+          Create
+        </motion.h1>
+        {/** Line 2 */}
+        <motion.div
+          style={{
+            x: '105%',
+          }}
+          className='location-hidden'
+        >
+          <p className='subtitle relative h-[20px] w-full'>MA, ES</p>
+        </motion.div>
+        {/** Line 3 */}
+        <motion.div className='with-no'>
+          <motion.h1 className='title col-span-4'>With No</motion.h1>
+        </motion.div>
+        {/** Line 4 */}
+        <motion.div style={{ x: '105%' }} className='limits-hidden'>
+          <motion.h1 className='title'>Limits</motion.h1>
+        </motion.div>
+        {/** Line 5 */}
+        <motion.div className='input-output' style={{ x: '50%' }}>
+          <motion.p
+            data-timeline-target=''
+            className='subtitle col-span-2 col-start-9'
+          >
+            0 -&gt; 1
+          </motion.p>
+        </motion.div>
+      </div>
+    </motion.div>
+  );
+};
+
+const BottomWall = () => {
+  return (
+    <motion.div
+      className='bottom-wall absolute left-0 w-full bg-white'
+      style={{
+        bottom: '0', // Red box bottom edge
+        height: 'calc(50% - 17rem)', // Distance from red box bottom to viewport bottom
+      }}
+    />
+  );
+};
+
+const TopWall = () => {
+  return (
+    <motion.div
+      // Top wall - covers from top of viewport to top of red box
+      style={{
+        y: 0,
+        height: 'calc(50% - 17rem)', // Distance from viewport top to red box top
+      }}
+      className='top-wall absolute top-0 left-0 w-full bg-white'
+    />
+  );
+};
+
+const LeftWall = () => {
+  return (
+    <>
+      <motion.div
+        className='left-wall absolute top-0 left-0 h-full bg-white'
+        style={{
+          width: 'calc(50% - 0px)',
+        }}
+      >
+        <div className='px-4'>
+          {/** Line 1 */}
+          <motion.div className='left-wall-separator' style={{ width: '100%' }}>
+            <motion.h1 className='create-title title text-right'>
+              Create
+            </motion.h1>
+            {/** Line 2 */}
+            <motion.div className='location subtitle relative col-span-4 col-start-5 h-[20px] w-full self-end'>
+              <motion.p
+                style={{
+                  right: '25%',
+                }}
+                className='absolute h-[fit-content]'
+              >
+                MA, ES
+              </motion.p>
+            </motion.div>
+            {/** Line 3 */}
+            <motion.div className='with-no-hidden'>
+              <motion.h1 style={{ x: '-100%' }} className='title col-span-4'>
+                With No
+              </motion.h1>
+            </motion.div>
+            {/** Line 4 */}
+            <motion.div
+              style={{ x: '0%' }}
+              className='limits pr-[128px] text-right'
+            >
+              <motion.h1 className='title'>Limits</motion.h1>
+            </motion.div>
+            {/** Line 5 */}
+            <motion.div className='input-output-hidden' style={{ x: '-100%' }}>
+              <motion.p
+                data-timeline-target=''
+                className='subtitle col-span-2 col-start-9'
+              >
+                0 -&gt; 1
+              </motion.p>
+            </motion.div>
+          </motion.div>
+        </div>
+      </motion.div>
+    </>
+  );
+};
+
+{
+  /* 
+        </div>
+        <div className='col-span-12 grid grid-cols-12'>
+
+        </div>
+        <div className='col-span-12 grid grid-cols-12 self-end'>
+          <motion.p
+            data-timeline-target='input-output'
+            style={{ x: '-160%' }}
+            className='subtitle col-span-2 col-start-2'
+          >
+            0 -&gt; 1
+          </motion.p>
+          <motion.p
+            data-timeline-target='input-output-hidden'
+            className='subtitle col-span-2 col-start-9'
+            style={{ x: '0%' }}
+          >
+            0 -&gt; 1
+          </motion.p>
+        </div>
+        <div className='col-span-12 grid grid-cols-12'>
+          <motion.h1
+            data-timeline-target='limits'
+            className='title col-span-4 col-start-3'
+          >
+            Limits
+          </motion.h1>{' '}
+          {/* <motion.h1
+            data-timeline-target='limits-hidden'
+            style={{ x: '100%' }}
+            className='title col-span-4 col-start-9'
+          >
+            Limits
+          </motion.h1>
+        </div>
+
+        <div className='col-span-12 grid grid-cols-12 self-end'>
+          <motion.p
+            data-timeline-target='copyright'
+            className='subtitle col-start-6'
+          >
+            © INC.
+          </motion.p>
+          <motion.p className='subtitle'>SINCE 1999</motion.p>
+        </div>
+      </div>
+      <div className='pointer-events-none absolute inset-0 flex h-screen w-screen'>
+        <motion.div
+          data-timeline-target='bg'
+          style={{ width: '0%', height: '75%' }}
+          className='m-auto overflow-hidden rounded-lg'
+        >
+          <img
+            className='inset-0 h-screen w-screen object-cover'
+            src='/img2.jpg'
+            alt='bg'
+          />
+        </motion.div>  */
 }
--- app/utils.ts
+++ app/utils.ts
@@ -1,141 +1,166 @@
-
 export const TRANSITION = {
-	duration: 1.5,
-	delay: 0.25,
-	ease: [0.76, 0, 0.24, 1],
-}
+  duration: 1.5,
+  delay: 0.25,
+  ease: [0.76, 0, 0.24, 1],
+};
 
 export const ANIMATIONS = [
-	{
-		name: 'keyframe-1',
-		animations: [{
-			target: 'create',
-			animation: {
-				x: '-25%',
-				y: 0,
-			}
-		},
-		{
-			target: 'input-output',
-			animation: {
-				x: '0%',
-			}
-		},
-		{
-			target: 'input-output-hidden',
-			animation: {
-				x: '300%',
-			}
-		},
-		{
-			target: 'location',
-			animation: {
-				x: '-125%',
-			}
-		},
-		{
-			target: 'location-hidden',
-			animation: {
-				x: '0%',
-			}
-		},
-		{
-			target: 'with-no-hidden',
-			animation: {
-				x: '0%',
-			}
-		},
-		{
-			target: 'with-no',
-			animation: {
-				x: '155%',
-			}
-		},
-		{
-			target: 'bg',
-			animation: {
-				width: '25%',
-			}
-		},
-		{
-			target: 'limits',
-			animation: {
-				x: '-120%',
-			}
-		},
-		{
-			target: 'limits-hidden',
-			animation: {
-				x: '-0%',
-			}
-		},
-		{
-			// target: 'with-no-hidden',
-			// animation: {
-			// 	x: '0%',
-			// }
-		},
-		{
-			target: 'limits',
-			animation: {
-				y: 0,
-			}
-		}]
-	},
-	{
-		name: 'keyframe-2',
-		animations: [
-			{
-			target: 'create',
-			animation: {
-				x: '-75%'
-			}
-		},
-		{
-			target: 'with-no-hidden',
-			animation: {
-				y: '-75%'
-			}
-		},
-		{
-			target: 'location-hidden',
-			animation: {
-				y: 'calc(-100vh / 5 * 1)'
-			}
-		}
-	]
-	}, {
-		name: 'keyframe-3',
-		animations: [
-			{
-				target: 'bg',
-				animation: {
-					height: '100%',
-					width: '100%'
-			}}
-		]
-	}
-]
+  {
+    name: 'keyframe-1',
+    animations: [
+      {
+        target: 'create',
+        animation: {
+          x: '-25%',
+          y: 0,
+        },
+      },
+      {
+        target: 'input-output',
+        animation: {
+          x: '0%',
+        },
+      },
+      {
+        target: 'input-output-hidden',
+        animation: {
+          x: '300%',
+        },
+      },
+      {
+        target: 'location',
+        animation: {
+          x: '-125%',
+        },
+      },
+      {
+        target: 'location-hidden',
+        animation: {
+          x: '0%',
+        },
+      },
+      {
+        target: 'with-no-hidden',
+        animation: {
+          x: '0%',
+        },
+      },
+      {
+        target: 'with-no',
+        animation: {
+          x: '155%',
+        },
+      },
+      {
+        target: 'bg',
+        animation: {
+          width: '25%',
+        },
+      },
+      {
+        target: 'limits',
+        animation: {
+          x: '-120%',
+        },
+      },
+      {
+        target: 'limits-hidden',
+        animation: {
+          x: '-0%',
+        },
+      },
+      {
+        // target: 'with-no-hidden',
+        // animation: {
+        // 	x: '0%',
+        // }
+      },
+      {
+        target: 'limits',
+        animation: {
+          y: 0,
+        },
+      },
+    ],
+  },
+  {
+    name: 'keyframe-2',
+    animations: [
+      {
+        target: 'create',
+        animation: {
+          x: '-75%',
+        },
+      },
+      {
+        target: 'with-no-hidden',
+        animation: {
+          y: '-75%',
+        },
+      },
+      {
+        target: 'location-hidden',
+        animation: {
+          y: 'calc(-100vh / 5 * 1)',
+        },
+      },
+    ],
+  },
+  {
+    name: 'keyframe-3',
+    animations: [
+      {
+        target: 'bg',
+        animation: {
+          height: '100%',
+          width: '100%',
+        },
+      },
+    ],
+  },
+];
 /** Utility fucntion to animate an element */
-export async function animateElement(animate: any, element: HTMLElement, animation: any, transition?: any) {
-	await animate(element, animation, transition = TRANSITION);
+export async function animateElement(
+  animate: any,
+  element: HTMLElement,
+  animation: any,
+  transition?: any
+) {
+  await animate(element, animation, (transition = TRANSITION));
 }
 
 /** This one executes animations at the same time  */
-export async function executeKeyframe(animate: any, elements: any, animations: any) {
-	const executableAnimations = animations.map((animation: any) => {
-		if (animation.target) {
-			return animateElement(animate, elements.current.get(animation.target), animation.animation, TRANSITION);
-		}
-		return Promise.resolve();
-	})
-	await Promise.all(executableAnimations);
+export async function executeKeyframe(
+  animate: any,
+  scope: any,
+  animations: any
+) {
+  const executableAnimations = animations.map((animation: any) => {
+    if (animation.target) {
+      const element = scope.current?.querySelector(
+        `[data-timeline-target="${animation.target}"]`
+      );
+      if (element) {
+        return animateElement(
+          animate,
+          element,
+          animation.animation,
+          TRANSITION
+        );
+      }
+    }
+    return Promise.resolve();
+  });
+  await Promise.all(executableAnimations);
 }
 
 /** Execute keyframes sequentially (one after another) */
-export async function timeline(animate: any, elements: any, animations = ANIMATIONS)
-{
-    for (const keyframe of animations) {
-		await executeKeyframe(animate, elements, keyframe.animations);
-	}
+export async function timeline(
+  animate: any,
+  scope: any,
+  animations = ANIMATIONS
+) {
+  for (const keyframe of animations) {
+    await executeKeyframe(animate, scope, keyframe.animations);
+  }
 }
